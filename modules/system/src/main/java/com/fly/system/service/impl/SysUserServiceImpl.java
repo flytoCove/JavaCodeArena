09024bba366c650c1a29936d42c13839420e0895
fly <12977083@qq.com>
2025-12-01 17:36:45 +0800

    管理员登录接口完善以及全局异常处理

--- modules/system/src/main/java/com/fly/system/service/impl/SysUserServiceImpl.java
+++ modules/system/src/main/java/com/fly/system/service/impl/SysUserServiceImpl.java
@@ -3,10 +3,10 @@ package com.fly.system.service.impl;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.fly.common.core.domain.R;
 import com.fly.common.core.enums.ResultCode;
-import com.fly.system.controller.LoginResult;
 import com.fly.system.domain.SysUser;
 import com.fly.system.mapper.SysUserMapper;
 import com.fly.system.service.ISysUserService;
+import com.fly.system.utils.BCryptUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -23,20 +23,26 @@ public class SysUserServiceImpl implements ISysUserService {
         SysUser sysUser = sysUserMapper.selectOne(queryWrapper.select(SysUser::getPassword)
                 .eq(SysUser::getUserAccount, userAccount));
         // 2.判断
-        R<Void> loginResult = new R<Void>();
+        // R<Void> loginResult = new R<Void>();
         if (sysUser == null) {
-            loginResult.setCode(ResultCode.FAILED_USER_NOT_EXISTS.getCode());
-            loginResult.setMsg(ResultCode.FAILED_USER_NOT_EXISTS.getMsg());
-            return loginResult;
+//            loginResult.setCode(ResultCode.FAILED_USER_NOT_EXISTS.getCode());
+//            loginResult.setMsg(ResultCode.FAILED_USER_NOT_EXISTS.getMsg());
+//            return loginResult;
+
+            return R.fail(ResultCode.FAILED_USER_NOT_EXISTS);
         }
-        if (!sysUser.getPassword().equals(password)) {
-            loginResult.setCode(ResultCode.FAILED_LOGIN.getCode());
-            loginResult.setMsg(ResultCode.FAILED_LOGIN.getMsg());
-            return loginResult;
+        if (!BCryptUtils.matchesPassword(password, sysUser.getPassword())) {
+//            loginResult.setCode(ResultCode.FAILED_LOGIN.getCode());
+//            loginResult.setMsg(ResultCode.FAILED_LOGIN.getMsg());
+//            return loginResult;
+
+            return R.fail(ResultCode.FAILED_LOGIN);
         }
 
-        loginResult.setCode(ResultCode.SUCCESS.getCode());
-        loginResult.setMsg(ResultCode.SUCCESS.getMsg());
-        return loginResult;
+//        loginResult.setCode(ResultCode.SUCCESS.getCode());
+//        loginResult.setMsg(ResultCode.SUCCESS.getMsg());
+//        return loginResult;
+
+        return R.ok();
     }
 }
